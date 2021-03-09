88dd334f55a419c4a8098dedbfbb823db94e1098
6135 <guibscosta@hotmail.com>
2021-03-09 11:49:11 +0000

    Runs

---
+++ build/external/glfw-3.3.3/examples/CMakeFiles/wave.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "RC"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_RC
+  "C:/Users/Gui/Documents/GitHub/CG/external/glfw-3.3.3/examples/glfw.rc" "C:/Users/Gui/Documents/GitHub/CG/build/external/glfw-3.3.3/examples/CMakeFiles/wave.dir/glfw.rc.obj"
+  )
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_RC
+  "GLEW_STATIC"
+  "TW_NO_DIRECT3D"
+  "TW_NO_LIB_PRAGMA"
+  "TW_STATIC"
+  "_CRT_SECURE_NO_WARNINGS"
+  )
+
+# The include file search paths:
+set(CMAKE_RC_TARGET_INCLUDE_PATH
+  "../external/glfw-3.3.3/deps"
+  "../external/glfw-3.3.3/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Gui/Documents/GitHub/CG/external/glfw-3.3.3/deps/glad_gl.c" "external/glfw-3.3.3/examples/CMakeFiles/wave.dir/__/deps/glad_gl.c.obj" "gcc" "external/glfw-3.3.3/examples/CMakeFiles/wave.dir/__/deps/glad_gl.c.obj.d"
+  "C:/Users/Gui/Documents/GitHub/CG/external/glfw-3.3.3/examples/wave.c" "external/glfw-3.3.3/examples/CMakeFiles/wave.dir/wave.c.obj" "gcc" "external/glfw-3.3.3/examples/CMakeFiles/wave.dir/wave.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/Gui/Documents/GitHub/CG/build/external/glfw-3.3.3/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/p1/CMakeFiles/p1.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Gui/Documents/GitHub/CG/p1/p1.cpp" "p1/CMakeFiles/p1.dir/p1.cpp.obj" "gcc" "p1/CMakeFiles/p1.dir/p1.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/Gui/Documents/GitHub/CG/build/external/glfw-3.3.3/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "C:/Users/Gui/Documents/GitHub/CG/build/external/glad/CMakeFiles/glad.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
