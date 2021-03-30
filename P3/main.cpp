59f5decb6d673dd66449737411125e76134f1cf7
6135 <guibscosta@hotmail.com>
2021-03-30 23:18:37 +0100

    moved

--- P3/main.cpp
+++ P3/main.cpp
@@ -92,8 +92,6 @@ void vertex_data_init(){
     glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
     glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,0,(void*)0);
 
-    
-
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, 0); 
     glBindVertexArray(0); 
