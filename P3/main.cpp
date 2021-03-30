6c08c6c8d45c378319f3d0d3d89d5159c8a37bfc
6135 <guibscosta@hotmail.com>
2021-03-30 16:44:37 +0100

    asd

--- P3/main.cpp
+++ P3/main.cpp
@@ -1,65 +1,49 @@
-
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 #include <iostream>
 #include <math.h>
-#define TITLE "Triangulo"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "gamma.cpp"
+
+
+#define TITLE "Gamma 3D"
 #define MAX_TIME_SECONDS 30
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput();
+
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec3 vertexColor;\n"
-    "out vec3 fragmentColor;\n"
     "uniform mat4 MVP;\n"
     "void main()\n""{\n"
     "   gl_Position = MVP * vec4(aPos, 1.0);\n"
-    "   fragmentColor = vertexColor;\n"
     "}\0";
 
 // declare and define fshader, position in color vector declaration
 // are RGBA from [0,1] simply in and out
 const char *fragmentShaderSource = "#version 330 core\n"
-"in vec3 fragmentColor;\n;"
-"out vec3 FragColor;\n"
-"void main()\n"
-"{\n"
-" FragColor = fragmentColor;\n"
-"}\n\0";
+    "out vec4 FragColor;\n"
+    "uniform vec4 ourColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = ourColor;\n"
+    "}\n\0";
+// settings
+
 
 unsigned int MatrixID;
 glm::mat4 Model = glm::mat4(1.0f);
 glm::mat4 View = glm::mat4(1.0f);
 glm::mat4 Projection = glm::mat4(1.0f);
 glm::mat4 MVP;
-const unsigned int SCR_WIDTH = 400;
-const unsigned int SCR_HEIGHT = 400;
-GLfloat red = 0.2f;
-GLfloat green = 0.2f;
-GLfloat blue = 0.2f;
+const unsigned int SCR_WIDTH = 900;
+const unsigned int SCR_HEIGHT = 900;
+GLfloat red = 0.6f;
+GLfloat green = 0.5f;
+GLfloat blue = 0.1f;
 GLfloat alpha = 1.0f;
-float vertices[] = {
-    -0.5f,  0.5f,  0.5f,  // left,up,front
-    0.5f,  0.5f,  0.5f,  // right,up,front
-    -0.5f,  0.5f, -0.5f,  // left,up,rear
-    -0.5f,  0.5f, -0.5f,  // left,up,rear
-    0.5f,  0.5f,  0.5f,  // right,up,front
-    0.5f,  0.5f, -0.5f   // right,up,rear
-};
-// One color for each vertex. Generated randomly.
-static const float colors[] = {
-0.722f, 0.645f, 0.174f,
-0.302f, 0.455f, 0.848f,
-0.225f, 0.587f, 0.040f,
-0.517f, 0.713f, 0.338f,
-0.053f, 0.959f, 0.120f,
-0.393f, 0.621f, 0.362f
-};
-
 
 unsigned int VBO, VAO, EBO;
 unsigned int shaderProgram, vertexShader,fragmentShader;
@@ -67,33 +51,29 @@ GLFWwindow* window;
 
 
 void vertex_data_init(){
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
-
-
-    glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glGenBuffers(1, &EBO);
 
-    Model = glm::rotate(Model, glm::radians(5.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
+
+    Model = glm::rotate(Model, glm::radians(0.0f), glm::vec3(1.0f, 0.0f, 0.0f));
     View = glm::translate(View, glm::vec3(0.0f, 0.0f, -3.0f));
     Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
     MVP = Projection * View * Model;
 
-    glBindBuffer(GL_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
-	glEnableVertexAttribArray(0);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,(void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
-    
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+    glBindVertexArray(0); 
 
-
-	glEnableVertexAttribArray(1);
-	glBindBuffer(GL_ARRAY_BUFFER, EBO);
-	glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,0,(void*)0);
 }
 
 void shader_setup(){
@@ -135,9 +115,6 @@ void shader_setup(){
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
         std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
     }
-
-
-
     // delete shaders, we don't need them anymore
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
@@ -174,6 +151,7 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SAMPLES,8);
 
 #ifdef __APPLE__
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
@@ -204,8 +182,10 @@ int main()
         glBindVertexArray(VAO);
         // seeing as we only have a single VAO there's no need to bind
         // it every time, but we'll do so to keep things a bit more organized
+        int vertex_color_location = glGetUniformLocation(shaderProgram,"ourColor");
+        glUniform4f(vertex_color_location,red,green,blue,1.0f);
         glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glDrawElements(GL_TRIANGLES, triangle_count ,GL_UNSIGNED_INT,0);
         // glBindVertexArray(0); // no need to unbind it every time 
         
         /* glfw: swap buffers and poll IO events (keys pressed/released, 
@@ -219,7 +199,7 @@ int main()
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
     // optional: de-allocate all resources once they've outlived their purpose:
-    // ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------        
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteProgram(shaderProgram);
@@ -229,7 +209,7 @@ int main()
 
 /* sets color specified in arguments
 -----------------------------------------------------------------------*/
-void glSetClearColor(GLfloat r, GLfloat g, GLfloat b,GLfloat a){
+void glSetColor(GLfloat r, GLfloat g, GLfloat b,GLfloat a){
   red = r;
   green = g;
   blue = b;
@@ -245,13 +225,13 @@ void processInput()
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 //   if(glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) {
-//     glSetClearColor(1.0f,0.0f,0.0f,1.0f);
+//     glSetColor(1.0f,0.0f,0.0f,1.0f);
 //   }
 //   if(glfwGetKey(window, GLFW_KEY_G) == GLFW_PRESS){
-//     glSetClearColor(0.0f,1.0f,0.0f,1.0f);
+//     glSetColor(0.0f,1.0f,0.0f,1.0f);
 //   }
 //   if(glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS){
-//       glSetClearColor(0.0f,0.0f,1.0f,1.0f);
+//       glSetColor(0.0f,0.0f,1.0f,1.0f);
 //   }
 }
 
