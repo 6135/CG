6b77c0a41efbcfa2c8b3a2779f5613195b02cd58
6135 <guibscosta@hotmail.com>
2021-04-29 17:48:53 +0100

    Revert "Update main.cpp"
    This reverts commit 3488bcddddfd8bb49bdad0877929fbb31d26b745.

--- TP3/main.cpp
+++ TP3/main.cpp
@@ -85,7 +85,11 @@ void vertex_data_init(){
     glBindBuffer(GL_ARRAY_BUFFER, EBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,0,(void*)0);
+
+    
+
 
 	glEnableVertexAttribArray(1);
 	glBindBuffer(GL_ARRAY_BUFFER, EBO);
